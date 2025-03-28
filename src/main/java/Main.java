7926adb79e32a36bfc10cefae400be41909f9045
Amr Samy <41027803+AmrSamy59@users.noreply.github.com>
2025-03-28 17:03:26 +0200

    Init with gradle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
-package Crawler;
+import Crawler.Crawler;
 
 public class Main {
     public static void main(String[] args) {
-        Crawler cr = new Crawler("seed.txt");
+        Crawler cr = new Crawler("src/seed.txt");
     }
 }
